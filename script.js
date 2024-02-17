14fc9d1ab9db2661cfe15c594ccad2b17f919415
VrakasYi <vrakasyi@outlook.com>
2024-02-17 17:47:03 +0200

    img src

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const searchForm = document.getElementById('searchForm');
+const text = document.getElementById('text');
 const temp = document.getElementById('temp');
 const windKph = document.getElementById('windKph');
 const windDir = document.getElementById('windDir')
+const img = document.getElementById('weatherIcon');
 
 async function getLocation(location) {    
   try {
@@ -9,11 +11,12 @@ async function getLocation(location) {
     (`https://api.weatherapi.com/v1/current.json?key=6ccb19deecb04d47a6793511240402&q=${location}`, {mode: 'cors'});
     const result = await response.json();
     console.log(result);
-    console.log(result.current.temp_c);
 
+    text.textContent = `${result.current.condition.text}`
     temp.textContent = `Temperature: ${result.current.temp_c} C`;
     windKph.textContent = `Wind speed: ${result.current.wind_kph}k/h`;
     windDir.textContent = `Wind direction: ${result.current.wind_dir}`
+    img.src = result.current.condition.icon
   } catch(error) {
     console.error('Error:', error);
   };
